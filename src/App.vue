2b7e39d0f77b6b607fa21a57d215b3e9e12a987c
hydra <hendryrpl@gmail.com>
2019-04-22 21:10:11 +0700

    created header and footer

--- src/App.vue
+++ src/App.vue
@@ -6,23 +6,43 @@
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios'
+import chart from './chartPie.js';
+import Header from './components/Header.vue'
+import Footer from './components/Footer.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    'app-header': Header,
+    'app-footer': Footer,
+    'app-chart': chart
+  },
+  async mounted(){
+    await axios.get('https://kpu.nyandev.id/api/v1/count').then((response)=>{
+      this.loaded = true
+      this.getData = [response.data.prabowo.percent, response.data.jokowi.percent]
+    })
+  },
+  data() {
+    return {
+      loaded: false,
+      detailData: null,
+      chartData: {
+        labels: ["PRABOWO-SANDI", "JOKOWI-AMIN"],
+        datasets: [
+          {
+            label: "Data One",
+            backgroundColor: ["#E46651", "#41B883"],
+            data: null
+          }
+        ]
+      }
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="header">
+        <i class="fas fa-chart-pie"></i>
+        <h4>Real Count KPU</h4>
+    </div>
+</template>
+
+<style>
+.header{
+    display: flex;
+    align-items:center;
+    position: fixed;
+    top: 0;
+    background: #3498db;
+    padding: 15px;
+    left: 0;
+    right: 0;
+}
+.header i{
+    margin-right: 10px;
+    color: #fff;
+    margin-bottom: 2px;
+}
+.header h4{
+    margin: 0;
+    color: #fff;
+}
+</style>
